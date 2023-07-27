c49545dbe323850df7a52d14f3b3d21096afc1d0
inhohwangg <sk1440sk@naver.com>
2023-07-27 23:55:53 +0900

    map link env 처리

--- lib/pages/map_test_page.dart
+++ lib/pages/map_test_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter/services.dart';
 import 'package:latlong2/latlong.dart';
@@ -61,8 +62,7 @@ class _HomePageState extends State<HomePage> {
       ),
       children: [
         TileLayer(
-          urlTemplate:
-              'http://api.vworld.kr/req/wmts/1.0.0/C76066FF-27A5-3837-9D15-DDB92635F8A3/Base/{z}/{y}/{x}.png',
+          urlTemplate: dotenv.env['FLUTTER_MAP_LINK'],
           userAgentPackageName: 'com.example.app',
         ),
         PolygonLayer(
