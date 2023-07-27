50637d4108d19c16dbebd5e008debce116ff2f2e
inhohwangg <sk1440sk@naver.com>
2023-07-28 00:31:32 +0900

    example page data add

--- lib/pages/example_page.dart
+++ lib/pages/example_page.dart
@@ -55,50 +55,157 @@ class _ExamplePageState extends State<ExamplePage> {
                 ),
               ),
               SizedBox(
-                height: 10,
+                height: 30,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Container(
-                    child: Text(controller.invateDataList[0]['groomName']),
+                    child: Text(controller.invateDataList[0]['groomName'],
+                        textScaleFactor: 2),
                   ),
                   Column(
                     children: [
                       Container(
-                        child: Text(formattedMonth),
+                        child: Text(formattedMonth, textScaleFactor: 2),
                       ),
-                      Text('ㅡ'),
+                      Text('ㅡ', textScaleFactor: 2),
                       Container(
-                        child: Text(formattedDay),
+                        child: Text(formattedDay, textScaleFactor: 2),
                       )
                     ],
                   ),
                   Container(
-                    child: Text(controller.invateDataList[0]['brideName']),
+                    child: Text(controller.invateDataList[0]['brideName'],
+                        textScaleFactor: 2),
                   ),
                 ],
               ),
               SizedBox(
-                height: 10,
+                height: 25,
               ),
               Container(
                 child: Text(
-                    '$formattedYear.$formattedMonth.$formattedDay ${controller.invateDataList[0]['weddingDayOfWeek']} ${controller.invateDataList[0]['weddingTime']}'),
+                    '$formattedYear.$formattedMonth.$formattedDay ${controller.invateDataList[0]['weddingDayOfWeek']} ${controller.invateDataList[0]['weddingTime']}',
+                    textScaleFactor: 2),
               ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(controller.invateDataList[0]['weddingVenue'],
+                  textScaleFactor: 1.5),
+              SizedBox(
+                height: 30,
+              ),
+              Text('INVITATION', textScaleFactor: 2),
               SizedBox(
                 height: 10,
               ),
-              Text(controller.invateDataList[0]['weddingVenue']),
+              Text('초대합니다.', textScaleFactor: 2),
               SizedBox(
-                height: 10,
+                height: 30,
               ),
-              Container(
-                height: 400,
+              Text('어제의 너와 내가 오늘을 우리가 되어', textScaleFactor: 1.2),
+              SizedBox(
+                height: 15,
+              ),
+              Text('저희 두 사람 이제 한길을 같이 걷고자 합니다.', textScaleFactor: 1.2),
+              SizedBox(
+                height: 15,
+              ),
+              Text('저희가 내딛는 첫 걸음에 부디 오셔서', textScaleFactor: 1.2),
+              SizedBox(
+                height: 15,
+              ),
+              Text('따뜻한 사랑으로 축복해 주시면 감사하겠습니다.', textScaleFactor: 1.2),
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 200,
                 child: Image.network(
                   controller.invateDataList[0]['invateImage'],
+                  fit: BoxFit.contain,
                 ),
               ),
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(controller.invateDataList[0]['groomFatherName'],
+                      textScaleFactor: 1.5),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('·'),
+                  Text(controller.invateDataList[0]['groomMotherName'],
+                      textScaleFactor: 1.5),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('의 아들'),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(controller.invateDataList[0]['groomName'],
+                      textScaleFactor: 1.5)
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(controller.invateDataList[0]['brideFatherName'],
+                      textScaleFactor: 1.5),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('·'),
+                  Text(controller.invateDataList[0]['brideMotherName'],
+                      textScaleFactor: 1.5),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('의 아들'),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(controller.invateDataList[0]['brideName'],
+                      textScaleFactor: 1.5)
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.05,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.brown[100]),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 15,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        '연락하기',
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
